d50f1f49c7655cdc39353fcc77e30b32f4f1dc81
Shelby Shuff <sshuff@mail.wvu.edu>
2015-07-08 15:10:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samp1::Application.config.secret_token = 'f7ffeaf7a3828cffdbd2cfc123fcc248f253896590fde288eb230656f3b6d4a3e11891188fe00dcb9ab5ad1276a4d66ffc0a751d799eaaba067abee0ab96c8e7'
